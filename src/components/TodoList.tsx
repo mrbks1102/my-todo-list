8063c19cea0de43e1735008874f313f8478e76c3
mrbks1102 <[mrbks1102@gmail.com]>
2020-12-11 20:20:26 +0900

    [add]コンポーネント作成

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Todoitem from './todoItem'
+import { Todo } from '../Types'
+
+type Props = {
+  todos: Todo[]
+  settodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const TodoList: React.FC<Props> = ({ todos, settodos }) => {
+
+  const handleDone = (todo: Todo) => {
+    settodos(prev => prev.map(t =>
+      t.id === todo.id
+        ? { ...todo, done: !todo.done }
+        : t
+    ))
+  }
+
+  const handleDelete = (todo: Todo) => {
+    settodos(prev => prev.filter(t =>
+      t.id !== todo.id
+    ))
+  }
+
+  return (
+    <div className="inner">
+      {
+        todos.length <= 0 ? '登録されたTODOはありません。' :
+          <ul className="todo-list">
+            {todos.map(todo => (
+              <Todoitem
+                key={todo.id}
+                todo={todo}
+                handleDelete={handleDelete}
+                handleDone={handleDone}
+              />
+            ))}
+          </ul>
+      }
+    </div>
+  )
+}
+
+export default TodoList
