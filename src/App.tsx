e05726637944d0ab5fb8068a2d9b40a26dfbba19
mrbks1102 <[mrbks1102@gmail.com]>
2020-12-11 20:03:15 +0900

    [add]型を定義

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Todo from './components/Todo'
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
-  const todo = [{ id: 'タスク1', text: '仕事をする' }]
+function App() {
   return (
     <div className="App">
-      <Todo items={todo} />
-      {/*itemsは、(todo)*/}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
